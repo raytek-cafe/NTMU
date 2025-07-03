6969ffe695295d9a8c7197e689eb83174f482be1
ImSwordQueen <52015098+ImSwordQueen@users.noreply.github.com>
2025-07-03 13:09:29 +0200

    Update ntmu.js

--- static/ntmu.js
+++ static/ntmu.js
@@ -9,7 +9,6 @@ var DATESTR_CONFIG = {
 
 function dateStrFromUTC(dateStr) {
     var date = new Date(dateStr + " UTC");
-    // Fallback for date formatting in IE
     return date.getFullYear() + '/' + 
            ('0' + (date.getMonth() + 1)).slice(-2) + '/' + 
            ('0' + date.getDate()).slice(-2) + ' ' + 
@@ -38,16 +37,51 @@ var page = {
         var data = {};
         var self = this; // Preserve context for async functions
 
+        var fetchData = function(url, successCallback, errorCallback) {
+            if (isIE) {
+                var xhr = new XMLHttpRequest();
+                xhr.open("GET", url, true);
+                xhr.onload = function() {
+                    if (xhr.status >= 200 && xhr.status < 300) {
+                        successCallback(xhr.responseText);
+                    } else {
+                        errorCallback();
+                    }
+                };
+                xhr.onerror = function() {
+                    errorCallback();
+                };
+                xhr.send();
+            } else {
+                fetch(url)
+                    .then(function(response) {
+                        if (!response.ok) throw new Error("Network response was not ok");
+                        return response.text();
+                    })
+                    .then(successCallback)
+                    .catch(errorCallback);
+            }
+        };
+
         switch (urlParts[0]) {
             case "":
                 template = "home";
-                this.loadData("data/packs.json?t=" + Date.now(), function(packs) {
-                    for (var i = 0; i < packs.length; i++) {
-                        packs[i].date = dateStrFromUTC(packs[i].date);
+                fetchData("data/packs.yml?t=" + Date.now(), function(packsText) {
+                    try {
+                        var packs = jsyaml.load(packsText);
+                        for (var i = 0; i < packs.length; i++) {
+                            packs[i].date = dateStrFromUTC(packs[i].date);
+                        }
+                        data.packs = packs;
+                        self.renderTemplate(template, data);
+                    } catch (e) {
+                        template = "error";
+                        data.message = "Failed to parse data for the packs list";
+                        self.renderTemplate(template, data);
                     }
-                    data.packs = packs;
-                    self.renderTemplate(template, data);
                 }, function() {
+                    template = "error";
+                    data.message = "Data request for packs list failed.";
                     self.renderTemplate(template, data);
                 });
                 break;
@@ -56,32 +90,34 @@ var page = {
                 var id = urlParts[1];
                 if (id === "" || id === undefined) break;
 
-                this.loadData("data/" + id + "/pack.json?t=" + Date.now(), function(json) {
-                    data.pack = json;
-                    data.pack.id = id;
+                fetchData("data/" + id + "/pack.yml?t=" + Date.now(), function(packDataText) {
+                    try {
+                        var packData = jsyaml.load(packDataText);
+                        data.pack = packData;
+                        data.pack.id = id;
 
-                    self.loadText("data/" + id + "/README.md?t=" + Date.now(), function(text) {
-                        if (!isIE) {
-                            // Only use commonmark if not in IE
+                        fetchData("data/" + id + "/README.md?t=" + Date.now(), function(readmeText) {
                             var parser = new commonmark.Parser();
                             var renderer = new commonmark.HtmlRenderer();
-                            var parsed = parser.parse(text);
+                            var parsed = parser.parse(readmeText);
                             data.pack.readme = renderer.render(parsed);
-                        } else {
-                            // Fallback for IE: just use the raw text or a simple conversion
-                            data.pack.readme = text.replace(/\n/g, "<br>"); // Simple line break conversion
-                        }
-
-                        for (var j = 0; j < data.pack.versions.length; j++) {
-                            data.pack.versions[j].date = dateStrFromUTC(data.pack.versions[j].date);
-                        }
-                        template = "pack";
+                            for (var j = 0; j < data.pack.versions.length; j++) {
+                                data.pack.versions[j].date = dateStrFromUTC(data.pack.versions[j].date);
+                            }
+                            template = "pack";
+                            self.renderTemplate(template, data);
+                        }, function() {
+                            template = "pack";
+                            self.renderTemplate(template, data);
+                        });
+                    } catch (e) {
+                        template = "error";
+                        data.message = "Failed to parse data for pack " + id;
                         self.renderTemplate(template, data);
-                    }, function() {
-                        self.renderTemplate("404", {});
-                    });
+                    }
                 }, function() {
-                    self.renderTemplate("404", {});
+                    template = "404";
+                    self.renderTemplate(template, data);
                 });
                 break;
         }
